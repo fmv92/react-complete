c6657f53cff78888f8e96ab30ee8e9a55e63733e
Fernando <fernando.mata.villalobos@gmail.com>
2022-07-16 13:28:53 -0600

    Modularizing ExpenseItem

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Groceries",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
@@ -29,22 +29,22 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
