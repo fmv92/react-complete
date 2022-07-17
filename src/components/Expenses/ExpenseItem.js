f0e530622d5210a101d3d7ad7207406c19fc30d8
Fernando <fernando.mata.villalobos@gmail.com>
2022-07-16 18:11:00 -0600

    Missing semicolon.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,9 +7,9 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    setTitle('Updated!');
-  }
-  
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
