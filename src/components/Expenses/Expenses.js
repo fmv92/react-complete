3a154f7cb13fd65332cb04fd1d50100418cebc4c
Fernando <fernando.mata.villalobos@gmail.com>
2022-07-20 08:17:13 -0600

    Added conditional return statements.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,31 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 function Expenses(props) {
   const [enteredYear, setEnteredYear] = useState("2020");
   const pickedYearDataHandler = (year) => {
-    console.log("Expenses.js "+ year);
+    console.log("Expenses.js " + year);
     setEnteredYear(year);
-
   };
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === enteredYear;});
-
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === enteredYear;
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter enteredYear={enteredYear} onYearPicked={pickedYearDataHandler} />
-        {filteredExpenses.map(item => (
-        <ExpenseItem
-        key={item.id} 
-        title={item.title} 
-        amount={item.amount} 
-        date={item.date} />
-        ))}
+        <ExpensesFilter
+          enteredYear={enteredYear}
+          onYearPicked={pickedYearDataHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
-  )
+  );
 }
 
 export default Expenses;
